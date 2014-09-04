46f4d307c5b083cc8de7c85894c3ab356ecf8a59
washingtoncampos <al.informatica.contato@gmail.com>
2014-09-03 21:41:13 -0400

    Adicionado as demais telas do cadastro da Sala; Fim da formatação do html

--- TDC/TDC.UI.Web/Controllers/SalaController.cs
+++ TDC/TDC.UI.Web/Controllers/SalaController.cs
@@ -3,7 +3,8 @@ using System.Web.Mvc;
 using TDC.UI.Web.Models;
 
 namespace TDC.UI.Web.Controllers
-{
+{ // TODO:Falta criar Editar e o Excluir
+
     public class SalaController : Controller
     {
 
@@ -13,11 +14,13 @@ namespace TDC.UI.Web.Controllers
             {
                 new Sala() {Nome = "Sala Minhoca"},
                 new Sala() {Nome = "Sala Sucupira"},
-                new Sala() {Nome = "Sala Castanheira"}
+                new Sala() {Nome = "Sala Castanheira"},
+                new Sala() {Nome = "Sala Bem-te-Vi"}
+
             };
             return View(lista);
         }
-        
+
         public ActionResult Cadastrar()
         {
             return View();
@@ -33,7 +36,77 @@ namespace TDC.UI.Web.Controllers
             return View(sala);
         }
 
+        public ActionResult Editar(string id)
+        {
+
+            {
+                //Busca no banco por Id
+                var sala = new Sala()
+                {
+                    Id = id,
+                    Nome = "Sala 001"
+                };
+
+                if (sala == null)
+                    return HttpNotFound();
+                return View(sala);
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Sala sala)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(sala);
+        }
+
+        public ActionResult Detalhe(string id)
+        {
+
+            {
+                //Busca no banco por Id
+                var sala = new Sala()
+                {
+                    Id = id,
+                    Nome = "Sala 001"
+                };
+
+                if (sala == null)
+                    return HttpNotFound();
+                return View(sala);
+
+            }
+
+        }
+
+        
+        public ActionResult Excluir (string id)
+        {
+
+            {
+                //Busca no banco por Id
+                var sala = new Sala()
+                {
+                    Id = id,
+                    Nome = "Sala 001"
+                };
+
+                if (sala == null)
+                    return HttpNotFound();
+                return View(sala);
+
+            }
+        }
+        //Exclui Registro
+        [HttpPost ,ActionName("Excluir")]
+        public ActionResult ConfirmarExcluir(string id)
+        {
+            return RedirectToAction("Index");
+        }
 
     }
 }
-
--- TDC/TDC.UI.Web/Models/Sala.cs
+++ TDC/TDC.UI.Web/Models/Sala.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TDC.UI.Web.Models
 {
     public class Sala:Entidade
     {
+
         [Required(ErrorMessage = "Preencha o Campo Nome da Sala")]
-        [StringLength(50,MinimumLength =  2,ErrorMessage = "O Nome da Sala deve possuir de 2 a 50 caracteres")]
-       public string Id { get; set; }
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O Campo Sala deve ter entre 2 e 30 caracteres")]
         public string Nome { get; set; }
 
     }
